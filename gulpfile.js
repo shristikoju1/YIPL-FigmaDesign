0fd3317aabec4226fbeaadd86c3bd1bb0024abf4
Shristi Koju <kojushristi4@gmail.com>
2024-07-24 13:44:30 +0545

    Organise the SCSS folder structure with clean and structure design

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,20 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const minify = require('gulp-minify');
 
-// Task to compile SCSS to CSS
 gulp.task('styles', () => {
-    return gulp.src('scss/*.scss') // Source SCSS files
-       .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-       .pipe(gulp.dest('css')); // Output CSS to the css folder
+    return gulp.src('scss/main.scss')  // Adjusted to point to main.scss
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(gulp.dest('css')); 
 });
 
-// Task to minify CSS files
 gulp.task('minify', () => {
-    return gulp.src('css/*.css') // Minify CSS files in the css folder
+    return gulp.src('css/*.css') 
         .pipe(minify({ ext: { min: '.min.css' } }))
-        .pipe(gulp.dest('css')); // Output minified CSS to the css folder
+        .pipe(gulp.dest('css')); 
 });
 
-// Default task to run both 'styles' and 'minify'
 gulp.task('default', gulp.series('styles', 'minify'));
 
-// Watch task to monitor changes in SCSS files
 gulp.task('watch', () => {
-    gulp.watch('scss/*.scss', gulp.series('styles'));
+    gulp.watch('scss/**/*.scss', gulp.series('styles', 'minify'));  // Watch for changes in all SCSS files
 });
